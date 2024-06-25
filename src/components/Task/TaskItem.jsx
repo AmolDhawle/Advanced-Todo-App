45144b12791234b72a47e0d55dca6b6c54264eef
Amol Dhawle <91775441+AmolDhawle@users.noreply.github.com>
2024-06-25 11:33:11 +0530

    Update TaskItem.jsx

--- src/components/Task/TaskItem.jsx
+++ src/components/Task/TaskItem.jsx
@@ -38,7 +38,7 @@ const TaskItem = ({ task, openModal, handleDelete }) => {
           className="bg-blue-500 text-white px-4 py-2 rounded ml-2"
           onClick={handleToggleCompletion}
         >
-          {task.completed ? 'Mark Incomplete' : 'Mark Complete'}
+          {task.completed ? 'Done' : 'Undone'}
         </button>
       </div>
     </div>
